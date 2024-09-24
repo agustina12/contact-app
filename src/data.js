73becc48859221a211cb6aa6fdbcfb6b6d973877
agustina <gustina200@gmail.com>
2024-09-24 15:25:43 +0800

    test

---
+++ src/data.js
@@ -0,0 +1,24 @@
+const getData = () => {
+  return [
+    {
+      id: 1,
+      name: "John Doe",
+      tag: "john",
+      imageUrl: "/images/john.png",
+    },
+    {
+      id: 2,
+      name: "Dua Lippa",
+      tag: "dua",
+      imageUrl: "/images/dua_lipa.png",
+    },
+    {
+      id: 3,
+      name: "Smith",
+      tag: "smith",
+      imageUrl: "/images/smith.png",
+    },
+  ];
+};
+
+export { getData };
