6eb9165de6e898815b5055e745a6585f21c90d2e
agustina <gustina200@gmail.com>
2024-10-01 14:39:48 +0800

    add new code

--- src/utils/data.js
+++ src/utils/data.js
@@ -4,19 +4,19 @@ const getData = () => {
       id: 1,
       name: "John Doe",
       tag: "john",
-      imageUrl: "/images/john.png",
+      imageUrl: "./images/john.png",
     },
     {
       id: 2,
       name: "Dua Lippa",
       tag: "dua",
-      imageUrl: "/images/dua_lipa.png",
+      imageUrl: "./images/dua_lipa.png",
     },
     {
       id: 3,
       name: "Smith",
       tag: "smith",
-      imageUrl: "/images/smith.png",
+      imageUrl: "./images/smith.png",
     },
   ];
 };
